b0f56ec34040869dfdadb035e6a67093cb2c8c66
Adrian Macneil <adrian@foxglove.dev>
2021-06-28 11:21:04 -0700

    Add type definitions

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ["plugin:@foxglove/base"],
   env: {
     browser: true,
+    es2020: true,
     node: true,
   },
 };
---
+++ browser.d.ts
@@ -0,0 +1,2 @@
+const fetch = typeof globalThis.fetch;
+export default fetch;
